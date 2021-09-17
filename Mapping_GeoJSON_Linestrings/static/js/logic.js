7f5981e92ed2ba10493c1731f1b0b13d972f72e5
Helene Charleux <helene.charleux@gmail.com>
2021-09-17 10:51:04 -0500

    Working with polygons

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,8 +1,8 @@
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+let map = L.map('mapid').setView([44.0, -80.0], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -11,15 +11,32 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/helenecharleux/Mapping_Earthquakes/main/torontoRoutes.json";
+
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/helenecharleux/Mapping_Earthquakes/main/majorAirports.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "lightyellow",
+  weight: 2
+}
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-    
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+      + feature.properties.dst + "</h3>");
+    }
+  })
+  
+.addTo(map);
 });
 
 
